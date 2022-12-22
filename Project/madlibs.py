4b01f31e147f6b50e1c395ad6b8aa657bd7d531a
hasan-abdullah-se <hasan.abdullah.se@gmail.com>
2022-12-23 00:48:07 +0600

    add project

---
+++ Project/madlibs.py
@@ -0,0 +1,8 @@
+# user guess the word and program give a text to fill the word
+adj = input("Give a Adjective ")
+verb1 = input("Give a name of your hobby ")
+verb2 = input("Again Give a name of your another hobby ")
+famous_person = input("Give a name of famous person you like to be ")
+madlibs  = f"Computer programming is so {adj}! It makes me so excited all the time beacuse \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlibs) 
